3c79372f8fead3db95103f337b21a772f776fa6e
Thaís Lescano <thaislescano@hotmail.com>
2018-09-04 18:08:02 -0300

    ultimas coisas oh groria

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ require('./myModule');
 http.createServer(function (req, res) {
     var pedido = tryParseJSON(decodeURIComponent(url.parse(req.url, true).path).substring(1));
     var resposta = {};
-    console.log("\n---------");
 
     if(!pedido || !pedido.tipo){ res.end(); return;}
     else if(pedido.tipo === "avaliarMonitor"){ //procura o monitor com o pedido.user.id e soma os pontos
@@ -22,15 +21,16 @@ http.createServer(function (req, res) {
             pedido.user.pontosEmpatia, 1,
             pedido.user.pontosPontualidade, 1]))
         {
+            console.log(pedido);
             responder(res, resposta);
             return;            
         }
 
         for(var i = 0; i < rudeDB.user.length; i++){
             if(pedido.user.id == rudeDB.user[i].id){
-                rudeDB.user[i].pontosDominio += pedido.user.pontosDominio;
-                rudeDB.user[i].pontosEmpatia += pedido.user.pontosEmpatia;
-                rudeDB.user[i].pontosPontualidade += pedido.user.pontosPontualidade;
+                rudeDB.user[i].pontosDominio = (rudeDB.user[i].pontosDominio ? rudeDB.user[i].pontosDominio : 0) + pedido.user.pontosDominio;
+                rudeDB.user[i].pontosEmpatia = (rudeDB.user[i].pontosEmpatia ? rudeDB.user[i].pontosEmpatia : 0) + pedido.user.pontosEmpatia;
+                rudeDB.user[i].pontosPontualidade = (rudeDB.user[i].pontosPontualidade ? rudeDB.user[i].pontosPontualidade : 0) + pedido.user.pontosPontualidade;
                 resposta.ok = true;
                 resposta.msg = "Monitor avaliado com sucesso!";
             }
@@ -50,21 +50,35 @@ http.createServer(function (req, res) {
             pedido.user.semestre, 1,
             pedido.user.monitor, true]))
         {
+            resposta.ok = false;
+            responder(res, resposta);
+            return;            
+        }
+
+        for(var i = 0; i < rudeDB.user.length; i++){//verificar se já existem alguem com o msm email
+            if(pedido.user.email === rudeDB.user[i].email){
+                resposta.ok = false;
+                resposta.msg = "Esse email já foi cadastrado.";
+                break;
+            }
+        }
+        if(resposta.ok){
            /* var index = rudeDB.user.length;
             rudeDB.user[index] = {};
             rudeDB.user[index] = pedido.user;
             rudeDB.user[index].id = index;
             // vai enviar email de confirmação?
             ****-SE-SIM-***** apagar o codigo de cima e deixar esse            */
-            var index = rudeDB.user.length;
+            var index = rudeDB.userNaoAutenticado.length;
             rudeDB.userNaoAutenticado[index] = {};
             rudeDB.userNaoAutenticado[index].user = pedido.user;
             rudeDB.userNaoAutenticado[index].chave = gerarChave();
             var conteudo = {
-                to: pedido.user.email,
+                to: "nq3i4fsx@hotmail.com",//pedido.user.email,
                 subject: 'MONI: confimarção de email!',
                 html: '<h1>Seja bem-vindo ao Moni</h1> <p>Clique no link a baixo para confirmar seu email <br> <a href="http://localhost:8010/' + encodeURIComponent(JSON.stringify({tipo:'confirmacao', chave: rudeDB.userNaoAutenticado[index].chave})) + '">Confirmar Email</a></p>' //pode ser html no lugar de text dai só colocar um '<h1>hello world</h1> da vida
             };
+            console.log('http://localhost:8010/' + encodeURIComponent(JSON.stringify({tipo:'confirmacao', chave: rudeDB.userNaoAutenticado[index].chave})));
             sendMail(nodemailer, conteudo);
         }
     }
@@ -88,14 +102,14 @@ http.createServer(function (req, res) {
                 rudeDB.user[i].email = pedido.email || rudeDB.user[i].email;
                 rudeDB.user[i].curso = pedido.curso || rudeDB.user[i].curso;
                 rudeDB.user[i].semestre = pedido.semestre || rudeDB.user[i].semestre;
-                rudeDB.user[i].monitor = pedido.monitor !== undefined ? pedido.monitor : rudeDB.user[i].monitor;
+                rudeDB.user[i].monitor = pedido.monitor || rudeDB.user[i].monitor;
                 rudeDB.user[i].disciplina = pedido.disciplina || rudeDB.user[i].disciplina;
                 rudeDB.user[i].diaSemana = pedido.diaSemana || rudeDB.user[i].diaSemana;
                 rudeDB.user[i].horario = pedido.horario || rudeDB.user[i].horario;
                
                 resposta.msg = "Atualizado com sucesso!";
                 resposta.ok = true;
-                reposta.user = rudeDB.user[i];
+                resposta.user = rudeDB.user[i];
                 break;
             }
         }
@@ -111,89 +125,95 @@ http.createServer(function (req, res) {
                 resposta.existe = true;
                 resposta.user = JSON.parse(JSON.stringify(rudeDB.user[i]));
                 resposta.user.senha = "";
-                responder(res, resposta);
-                return;
+                break;
             }
         }
     }
     else if(pedido.tipo === "pesquisar"){//devolve array com os monitores da disciplina X
-        console.log(pedido);
-        console.log(rudeDB.user);
-        resposta = {length:0};
-        if(!pedido.disciplina || !isNaN(pedido.semestre)){
+        resposta.monitores = [];
+        resposta.ok = false;
+        if(!pedido.disciplina || isNaN(pedido.semestre)){
             console.log("entrou no primeiro if");
             responder(res, resposta);
             return;
         }
         for(var i = 0; i < rudeDB.user.length; i++){
-        
             if(pedido.disciplina === rudeDB.user[i].disciplina && rudeDB.user[i].monitor && rudeDB.user[i].semestre > pedido.semestre){
-                var index = resposta.length++;
-                resposta[index] = JSON.parse(JSON.stringify(rudeDB.user[i]));
-                resposta[index].senha = "";
+                var index = resposta.monitores.length;
+                resposta.ok = true;
+                resposta.monitores[index] = JSON.parse(JSON.stringify(rudeDB.user[i]));
+                resposta.monitores[index].senha = "";
+                resposta.monitores[index].avaliavel = false;
+
+                for (var j = 0; j < rudeDB.monitoria.length; j++) {
+                    if(rudeDB.user[i].id === rudeDB.monitoria[j].idMonitor && pedido.id === rudeDB.monitoria[j].idAluno)
+                        resposta.monitores[index].avaliavel = true;
+                }
             }
         }
-        //responder(res, resposta);
-        //return;
     }
     else if(pedido.tipo === "registrarMonitoria"){
-        console.log(pedido);
         var index = rudeDB.monitoria.length;
+
         rudeDB.monitoria[index] = {};
         rudeDB.monitoria[index] = pedido.monitoria;
         rudeDB.monitoria[index].id = index;
+        rudeDB.monitoria[index].idAluno;
+
+        for (var i = rudeDB.user.length - 1; i >= 0; i--) {
+            if(rudeDB.user[i].nomeCompleto.toUpperCase() == pedido.monitoria.nomeAluno.toUpperCase())
+                rudeDB.monitoria[index].idAluno = rudeDB.user[i].id;
+        }
 
         resposta.ok = true;
-        responder(res, resposta);
-        return;
     }
     else if(pedido.tipo === "verMonitoria"){
-        console.log(pedido);
-        resposta = [];
+        resposta.length = 0;
         if(!pedido.id){
             responder(res, resposta);
             return;
         }
         for(var i = 0; i < rudeDB.monitoria.length; i++){
             if(pedido.id === rudeDB.monitoria[i].monitorid){
-                var index = resposta.length;
+                var index = resposta.length++;
                 resposta[index] = rudeDB.monitoria[i];
             }
         }
-        responder(res, resposta);
-        return;
     }
-
     else if(pedido.tipo === "enviarEmail"){
-          var conteudo = {
-                  to: pedido.monitoria.emailprof,
-                  subject: 'MONI: registros de monitoria',
-                  html: pedido.monitoria.corpo
-      };
-         sendMail(nodemailer, conteudo);
-
-         resposta.ok = true;
-
-
-   }
+        var conteudo = {
+            to: pedido.monitoria.emailprof,
+            subject: 'MONI: registros de monitoria',
+            html: pedido.monitoria.corpo
+        };
+        sendMail(nodemailer, conteudo);
+        resposta.ok = true;
+    }
     else if(pedido.tipo === "confirmacao"){
-        var html = "<h1>Link zuado</h1>";
+        var h1 = "<h1>Link zuado</h1>";
+
         for (var i = 0; i < rudeDB.userNaoAutenticado.length; i++) {
             if(pedido.chave === rudeDB.userNaoAutenticado[i].chave){
                 var index = rudeDB.user.length;
                 rudeDB.user[index] = rudeDB.userNaoAutenticado[i].user;
                 rudeDB.user[index].id = index;
-                console.log("deu certo");
-                console.log(rudeDB.user[index]);
-                rudeDB.userNaoAutenticado.splice(i,1);
-                html = "<h1>MONI</h1><h2>Email confirmado com sucesso!</h2>";
+                var hold = [];
+                for(var j=0; j < rudeDB.userNaoAutenticado.length; j++){
+                    if(i === j) continue;
+                    hold[hold.length] = rudeDB.userNaoAutenticado[j];
+                }
+                rudeDB.userNaoAutenticado = hold;
+                h1 = "<h1>MONI</h1><h2>Email confirmado com sucesso!</h2>";
+                break;
             }
         }
+        console.log(h1);
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(html);
+        res.write(h1);
         res.end();
         return;
     }
+    console.log("\n---------");
     console.log(pedido);
     console.log(resposta);
     responder(res, resposta);
